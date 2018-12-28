57203df726f8f854b2b51b56a500067adb147708
Will Sargent <will@tersesystems.com>
2018-12-28 10:22:16 -0800

    change publishTo

--- project/CommonSettings.scala
+++ project/CommonSettings.scala
@@ -20,14 +20,15 @@ object CommonSettings extends AutoPlugin {
     javacOptions in (Compile, doc) ++= javacSettings,
     javacOptions in Test ++= javacSettings,
     publishConfiguration := publishConfiguration.value.withOverwrite(false),
+    publishTo := Some(
+      Resolver.file("file", new File(baseDirectory.value + "release-repo"))),
     // docker pull docker.bintray.io/jfrog/artifactory-oss:latest
     // docker run --name artifactory -d -p 8081:8081 docker.bintray.io/jfrog/artifactory-oss:latest
     // open browser to http://localhost:8081/
     // skip password, set up sbt repo.
     // https://www.jfrog.com/confluence/display/RTF/SBT+Repositories#SBTRepositories-DeployingArtifacts
     // admin / password
-    publishTo := Some(
-      "Artifactory Realm" at "http://localhost:8081/artifactory/sbt-release-local"),
+    //  publishTo := Some ("Artifactory Realm" at "http://localhost:8081/artifactory/sbt-release-local"),
     credentials += Credentials("Artifactory Realm",
                                "localhost",
                                "admin",
